df83b77e3876121c1fd228f06125d910282f8db2
Kate <katyamalysheva2000@yandex.ru>
2022-09-19 15:30:34 +0300

    added single wallet get and delete functionality

--- PayKate/WalletService/views.py
+++ PayKate/WalletService/views.py
@@ -2,7 +2,13 @@
 """
 from django.contrib.auth.models import User
 from rest_framework import permissions
-from rest_framework.generics import CreateAPIView, ListAPIView, ListCreateAPIView
+from rest_framework.generics import (
+    CreateAPIView,
+    ListAPIView,
+    ListCreateAPIView,
+    RetrieveDestroyAPIView,
+)
+from rest_framework.response import Response
 from WalletService.models import Wallet
 
 from .serializers import UserRegisterSerializer, WalletSerializer
@@ -38,3 +44,22 @@ class WalletListView(ListCreateAPIView):
     def perform_create(self, serializer):
         """Transmits user data to serializer create method"""
         return serializer.save(user=self.request.user)
+
+
+class WalletDetailView(RetrieveDestroyAPIView):
+    """API view for a single wallet (supports get and delete)"""
+
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = WalletSerializer
+    lookup_field = "name"
+
+    def get_queryset(self):
+        """Gets user wallets"""
+        user = self.request.user
+        return Wallet.objects.filter(user=user)
+
+    def delete(self, request, *args, **kwargs):
+        "Deletes user's wallet"
+        name = kwargs["name"]
+        response = super().delete(request, *args, **kwargs)
+        return Response(f"Wallet {name} deleted", status=response.status_code)

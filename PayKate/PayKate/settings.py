afd0ab9aa55f52b2910b77071c7ba33c294cd8f8
Kate <katyamalysheva2000@yandex.ru>
2022-09-20 22:12:31 +0300

    changed db to postgresql

--- PayKate/PayKate/settings.py
+++ PayKate/PayKate/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 env = Env()
 env.read_env(os.path.join(BASE_DIR, ".env"))
-SECRET_KEY = "SECRET_KEY"
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -81,8 +81,12 @@ WSGI_APPLICATION = "PayKate.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env("POSTGRES_DB"),
+        "USER": env("POSTGRES_USER"),
+        "PASSWORD": env("POSTGRES_USER_PSWD"),
+        "HOST": "localhost",
+        "PORT": 5432,
     }
 }
 
